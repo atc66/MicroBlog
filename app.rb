d10886dc64d1353fd4a79eae4cd260dfb5dd848a
Ty Thompson <tylermoko@gmail.com>
2018-02-14 17:02:18 -0500

    Added list all blogs page, links to each blog

--- app.rb
+++ app.rb
@@ -8,3 +8,14 @@ require './models'
 get '/' do 
 	erb :index
 end
+
+get '/blogs' do
+	@blogs = Blog.all
+		
+	erb :"/blogs/blogs"
+end
+
+get '/blogs/:id' do
+	@blog = Blog.find(params[:id])
+	erb :"blogs/blog"
+	end
