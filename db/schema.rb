60d9ee588e1bd487d9f4dfc16ac54ab87386643c
SheilaWaples <sheilawaples78@gmail.com>
2018-02-14 16:01:01 -0500

    created blogs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214203616) do
+ActiveRecord::Schema.define(version: 20180214205027) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "userid"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- models.rb
+++ models.rb
@@ -1,2 +1,5 @@
 class User<ActiveRecord::Base
 end
+
+class Blog<ActiveRecord::Base
+	end
