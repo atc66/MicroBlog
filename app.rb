495e98979529b37affed6797686cae38eba48885
Ty Thompson <tylermoko@gmail.com>
2018-02-15 11:03:31 -0500

    added blogs to homepage

--- app.rb
+++ app.rb
@@ -2,16 +2,17 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 set :database, "sqlite3:main.sqlite3"
+set :sessions, true
 
 require './models'
 
 get '/' do 
+	@blogs = Blog.all
 	erb :index
 end
 
 get '/blogs' do
 	@blogs = Blog.all
-		
 	erb :"/blogs/blogs"
 end
 
