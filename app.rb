2031e075f48e2ac04caacb2b180c5e868f6a2e01
SheilaWaples <sheilawaples78@gmail.com>
2018-02-15 10:28:14 -0500

    correctd sign up page

--- app.rb
+++ app.rb
@@ -24,13 +24,3 @@ get '/users' do
 	erb :users
 end
 
-post '/' do
-user = User.where(username: params[:username]).first
-
-if user.username == params[:username]
-	session[:user_id]=user.id
-	redirect '/profile'
-else
-	redirect '/'
-	end
-end
